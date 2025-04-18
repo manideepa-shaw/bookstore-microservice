a9313c676782622ed598f2436711872d2dc124e7
Manideepa Shaw <jiyamanipriya@gmail.com>
2025-04-18 17:16:57 +0530

    Initial commit

---
+++ src/test/java/com/bookstore/catalog/TestCatalogApplication.java
@@ -0,0 +1,11 @@
+package com.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
