56dfef4061c176c8d1b380fa62a04259d8cd92b9
Manideepa Shaw <jiyamanipriya@gmail.com>
2025-04-19 13:35:38 +0530

    skeleton of catalog-service made

--- src/test/java/com/bookstore/catalog/TestcontainersConfiguration.java
+++ src/test/java/com/bookstore/catalog/TestcontainersConfiguration.java
@@ -9,10 +9,9 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"));
-	}
-
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8.3.0"));
+    }
 }
